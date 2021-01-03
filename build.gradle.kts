896132b9d0b55f9860bf7e50cbe673962201bf5b
Annapurna Pingale <pannapur@thoughtworks.com>
2021-01-03 14:40:53 +0530

    [Annapurna] | Modified the post endpoint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.1"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+    id("org.springframework.boot") version "2.4.1"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
 }
 
 group = "com.example"
@@ -12,25 +12,26 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/com/example/remotedebugapp/employee/EmployeeController.kt
+++ src/main/kotlin/com/example/remotedebugapp/employee/EmployeeController.kt
@@ -3,14 +3,15 @@ package com.example.remotedebugapp.employee
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/employeeDetails")
 class EmployeeController(val employeeService: EmployeeService) {
 
-    @GetMapping("/employeeDetails")
+    @GetMapping
     fun getEmployeeDetails(): List<Employee> {
         return employeeService.getEmployeeDetails()
     }
 
-    @PostMapping("/addEmployee")
+    @PostMapping
     fun addEmployee(@RequestBody employee: Employee): List<Employee> {
         return employeeService.setEmployee(employee)
     }
